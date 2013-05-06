7a9a0c7f02f65590fbd3247c5d676ddef86a07ce
Elvio Toccalino <etoccalino@fi.uba.ar>
2013-05-05 22:41:00 -0300

    simplest packaget

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="gevent_test",
+    version="0.1.0",
+    packages=['gevent_test'],
+
+    description="Testing utiliites for greenlet driven code.",
+    # long_description=open('README.txt').read(),
+
+    license="",
+
+    # test_suite='',
+
+    # Metadata for upload to PyPI.
+    author="Elvio Toccalino",
+    author_email="elvio.toccalino@gmail.com",
+    keywords="testing test unittest gevent greenlet",
+    url="",
+)
