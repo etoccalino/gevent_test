7f732f7c3b94084d6cc87c6cee138734d51e67bb
Elvio Toccalino <etoccalino@fi.uba.ar>
2013-05-05 22:47:40 -0300

    moved test code into a proof-of-concept dir.

--- proof-of-concept/tests.py
+++ proof-of-concept/tests.py
@@ -1,36 +1,36 @@
-import synctest
-import target
-import complex_target
+import gevent_test
+import tasks
+import related
 
 
 def ending_task_is_done(self):
     return self.greenlet.ready()
 
-target.EndingTask.is_done = ending_task_is_done
+tasks.EndingTask.is_done = ending_task_is_done
 
 
-class TaskTestCase (synctest.SyncTestCase):
+class TaskTestCase (gevent_test.SyncTestCase):
 
     def test_ending_task(so):
-        task = target.EndingTask()
+        task = tasks.EndingTask()
         task.start()
         so.when(task.is_done).assertEqual(task.result, 'done!')
 
     def test_persistent_task(so):
-        task = target.PersistentTask()
+        task = tasks.PersistentTask()
         task.start()
         so.when(task.is_idling).assertTrue(task.waiting_for_resource)
 
 
-class ComplexTestCase (synctest.SyncTestCase):
+class RelatedTestCase (gevent_test.SyncTestCase):
 
     def test_task(so):
-        cpu = complex_target.get_cpu()
+        cpu = related.get_cpu()
         cpu.remove_all_processors()
         cpu.new_processor_for('some-robot')
 
         processor = cpu.processors['some-robot']
-        job = complex_target.Job()
+        job = related.Job()
 
         (so.after(cpu.submit, job).when(processor.is_idling)
-         .assertEqual(job.result, complex_target.Job.SUCCESS))
+         .assertEqual(job.result, related.Job.SUCCESS))
