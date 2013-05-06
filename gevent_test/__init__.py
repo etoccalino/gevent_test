0076115967c375507ebe7910194aec35347f4c83
Elvio Toccalino <etoccalino@fi.uba.ar>
2013-05-05 23:47:00 -0300

    added docstrings to methods.

--- gevent_test/__init__.py
+++ gevent_test/__init__.py
@@ -4,13 +4,15 @@ import unittest
 
 class SyncTestCase (unittest.TestCase):
 
-    def when(self, prop, *args, **kwargs):
-        condition = prop
-        # if hasattr(prop, 'is_idling') and callable(prop.is_idling):
-        #     condition = prop.is_idling
+    def when(self, condition, *args, **kwargs):
+        """Wait until the condition is met before proceeding.
 
+        Returns the test instance, to allow chaining an assert method.
+
+        The `condition` must be a callable, returning truthy-falsy.
+        """
         if not callable(condition):
-            raise ValueError('first argmument must be... nicer')
+            raise ValueError('first argmument must be a callable proposition')
 
         while not condition(*args, **kwargs):
             gevent.sleep()
@@ -18,8 +20,14 @@ class SyncTestCase (unittest.TestCase):
         return self
 
     def after(self, action, *args, **kwargs):
+        """Wait until the action finishes before proceeding.
+
+        Returns the test instance, to allow chaining an assert method.
+
+        The `condition` must be a callable, returning truthy-falsy.
+        """
         if not callable(action):
-            raise ValueError('first argmument must be... nicer')
+            raise ValueError('first argmument must be a callable')
 
         gevent.spawn(action, *args, **kwargs).join()
 
